43a5d0ab026d51e3c94c86909e83bcbf2d5f0962
nicolas <nicolasbernard19@gmail.com>
2026-03-24 11:50:32 +0000

    setup header and userList components

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,23 @@
+
+import { ClerkProvider, Show, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
+
+export async function Header() {
+
+    return (
+        // <ClerkProvider>
+        <header className="flex justify-end items-center p-4 gap-4 h-16">
+            <Show when="signed-out">
+              <SignInButton />
+              <SignUpButton>
+                <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                  Sign Up
+                </button>
+              </SignUpButton>
+            </Show>
+            <Show when="signed-in">
+              <UserButton />
+            </Show>
+          </header>
+        // </ClerkProvider>
+    )
+}
